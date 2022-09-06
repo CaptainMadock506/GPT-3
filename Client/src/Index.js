6c6cd4b906b09443467f8764953732d98d3ea37d
Ashutosh@kail <anonymous.coder506@gmail.com>
2022-09-06 17:13:50 -0400

    Adding the Client folder

---
+++ Client/src/Index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import './Index.css'
+
+const Root = document.getElementById("root");
+ReactDOM.render( <App /> , Root)
